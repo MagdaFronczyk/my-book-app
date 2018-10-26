910fa23a92a704249e85662ab01833f0314c80ff
Magdalena Fronczyk <magdalenaoliwiaf@gmail.com>
2018-10-26 12:39:51 +0200

    added docs folder

---
+++ docs/precache-manifest.de7f95634ec7aeb44ae2c30c6311f247.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "84d56d4eb4ed99a58e77",
+    "url": "./static/js/main.84d56d4e.chunk.js"
+  },
+  {
+    "revision": "608312dcc0d3b60f8147",
+    "url": "./static/js/1.608312dc.chunk.js"
+  },
+  {
+    "revision": "84d56d4eb4ed99a58e77",
+    "url": "./static/css/main.59013526.chunk.css"
+  },
+  {
+    "revision": "356f4f52dbe6ed7f78fb1c9e29e778d7",
+    "url": "./index.html"
+  }
+];
--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
 
 class BookItem extends React.Component {
-   constructor() {
+    constructor() {
         super();
         this.state = {
             bookshelf: "",
             disabledRead: null,
             disabledToRead: null,
         }
-   }
+    }
 
     handleSelect = (e, element) => {
         const shelf = e.target.value === "Read" ? "read" : "toRead";
@@ -18,53 +18,53 @@ class BookItem extends React.Component {
         this.props.setShelf(shelf, element)
     };
 
-   handleRead = (e) => {
-       this.setState({
-           disabledRead: true,
-       })
-   };
+    handleRead = (e) => {
+        this.setState({
+            disabledRead: true,
+        })
+    };
 
-   handleToRead = (e) => {
-       this.setState({
-           disabledToRead: true,
-       })
-   };
+    handleToRead = (e) => {
+        this.setState({
+            disabledToRead: true,
+        })
+    };
 
-   render(){
+    render() {
 
-       console.log(this.state.disabledRead);
-       console.log(this.state.disabledToRead);
+        console.log(this.state.disabledRead);
+        console.log(this.state.disabledToRead);
 
-       const element = this.props.book;
-       const index = this.props.index;
-     return (
-         <li>
-             <div className="bestsellers_main_panel_books_bookList_item_info">
-                 <p className="bestsellers_main_panel_books_bookList_rank">#{index + 1}</p>
-                 <select
-                     value={this.state.bookshelf}
-                     id="addNewBook"
-                     className="bestsellers_main_panel_books_bookList_item_button"
-                     onChange={event => this.handleSelect(event, element)}>
-                     <option>Add:</option>
-                     <option onClick={this.handleRead} disabled={this.state.disabledRead} >Read</option>
-                     <option onClick={this.handleToRead} disabled={this.state.disabledToRead}>To Read</option>
-                 </select>
+        const element = this.props.book;
+        const index = this.props.index;
+        return (
+            <li>
+                <div className="bestsellers_main_panel_books_bookList_item_info">
+                    <p className="bestsellers_main_panel_books_bookList_rank">#{index + 1}</p>
+                    <select
+                        value={this.state.bookshelf}
+                        id="addNewBook"
+                        className="bestsellers_main_panel_books_bookList_item_button"
+                        onChange={event => this.handleSelect(event, element)}>
+                        <option>Add:</option>
+                        <option onClick={this.handleRead} disabled={this.state.disabledRead}>Read</option>
+                        <option onClick={this.handleToRead} disabled={this.state.disabledToRead}>To Read</option>
+                    </select>
 
-                 <p className="bestsellers_main_panel_books_bookList_item_title">
-                     <i>{element.book_details[0].title}</i></p>
-                 <p className="bestsellers_main_panel_books_bookList_item_author">
-                     <i>by {element.book_details[0].author}</i></p>
-                 <p className="bestsellers_main_panel_books_bookList_item_description">{element.book_details[0].description}</p>
-                 <div>
-                     <p className="bestsellers_main_panel_books_bookList_item_weeks"><i><span>Weeks on list:</span> {element.weeks_on_list}</i></p>
-                     <a href={element.amazon_product_url} target="_blank" className="bestsellers_main_panel_books_bookList_item_link">Buy on amazon</a>
-                 </div>
+                    <p className="bestsellers_main_panel_books_bookList_item_title">
+                        <i>{element.book_details[0].title}</i></p>
+                    <p className="bestsellers_main_panel_books_bookList_item_author">
+                        <i>by {element.book_details[0].author}</i></p>
+                    <p className="bestsellers_main_panel_books_bookList_item_description">{element.book_details[0].description}</p>
+                    <p className="bestsellers_main_panel_books_bookList_item_weeks">
+                        <i><span>Weeks on list:</span> {element.weeks_on_list}</i></p>
+                    <a href={element.amazon_product_url} target="_blank"
+                       className="bestsellers_main_panel_books_bookList_item_link">Buy on amazon</a>
+                </div>
+            </li>
 
-             </div>
-         </li>
+        )
+    }
+}
 
-     )
-   }
- }
- export default  BookItem;
+export default BookItem;
